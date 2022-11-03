2e27442a4d97d67ae01317e8b23f86b5ce93fd1c
Prince Codes <princecodes247@gmail.com>
2022-11-03 04:49:00 +0100

    feat: add error handler

--- bin/commands/generate.js
+++ bin/commands/generate.js
@@ -4,27 +4,31 @@ const csvToJSON = require("../utils/convertCSVToJSON");
 const jsonToCSV = require("../utils/convertJSONtoCSV");
 
 function generate(path) {
-  //   Convert CSV file to JSON
-  console.log("Converting CSV to JSON...");
-  const csvFile = fs.readFileSync(path, "utf-8");
+  try {
+    //   Convert CSV file to JSON
+    const csvFile = fs.readFileSync(path, "utf-8");
+    console.log("Converting CSV to JSON...");
 
-  const json = csvToJSON(csvFile.toString());
+    const json = csvToJSON(csvFile.toString());
 
-  //   Generate a hash for each object in the JSON array
-  json.forEach((obj) => {
-    const hash = crypto.createHash("sha256");
-    hash.update(JSON.stringify(obj));
-    obj.HASH = hash.digest("hex");
-  });
+    //   Generate a hash for each object in the JSON array
+    json.forEach((obj) => {
+      const hash = crypto.createHash("sha256");
+      hash.update(JSON.stringify(obj));
+      obj.HASH = hash.digest("hex");
+    });
 
-  //   Convert JSON array to CSV
-  const newCSV = jsonToCSV(json);
+    //   Convert JSON array to CSV
+    const newCSV = jsonToCSV(json);
 
-  //   Create a new CSV file with the hash appended to the filename
-  const newFileName = new String(path).slice(0, -4) + ".output.csv";
-  fs.writeFileSync(newFileName, newCSV);
+    //   Create a new CSV file with the hash appended to the filename
+    const newFileName = new String(path).slice(0, -4) + ".output.csv";
+    fs.writeFileSync(newFileName, newCSV);
 
-  console.log(`New CSV file created: ${newFileName}`);
+    console.log(`New CSV file created: ${newFileName}`);
+  } catch (error) {
+    console.error("File not found or supported");
+  }
 }
 
 module.exports = generate;
